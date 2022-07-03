7595667e9dba68b0e912365c5cc22a4d620b9638
Qúi <95676025+qui123456qqq@users.noreply.github.com>
2022-07-03 21:47:52 +0700

    Update TamGiacPascal.java

--- DeNL_2013-2014/src/Ex_2/TamGiacPascal.java
+++ DeNL_2013-2014/src/Ex_2/TamGiacPascal.java
@@ -5,7 +5,28 @@ public class TamGiacPascal {
         if (k == 0 || k == n) return 1;
         return toHop(n-1,k-1) + toHop(n-1,k);
     }
-
+private static void tamGiacPascal() {
+        int n = 5;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                int iGT = 1;
+                for (int k = 1; k <= i; k++) {
+                    iGT = k * iGT;
+                }
+                int jGT = 1;
+                for (int k = 1; k <= j; k++) {
+                    jGT = k * jGT;
+                }
+                int ijGT = 1;
+                for (int k = 1; k <= (i - j); k++) {
+                    ijGT = k * ijGT;
+                }
+                int toHop = (iGT / (jGT * ijGT));
+                System.out.print(toHop + "\t");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         int n = 5;
         for (int i = 0; i < n; i++) {
@@ -14,5 +35,7 @@ public class TamGiacPascal {
             }
             System.out.println();
         }
+        System.out.println();
+        tamGiacPascal();
     }
 }
